3a148a5073a992e7819ed3f414506f51f3ddd296
Gwynne Raskind <gwynne@darkrainfall.org>
2026-02-19 01:33:52 -0600

    chore: Revert "feat: don't generate JS exports in extract mode"
    This reverts commit b4e3741192f2d07764021c4ab58870ddcca26034.

--- src/loaders/postcss/index.ts
+++ src/loaders/postcss/index.ts
@@ -104,14 +104,17 @@ const loader: Loader<PostCSSLoaderOptions> = {
           this.warn({ plugin: msg.plugin, message: msg.text as string });
           break;
         }
+
         case "icss": {
           Object.assign(modulesExports, msg.export as Record<string, string>);
           break;
         }
+
         case "dependency": {
           this.deps.add(normalizePath(msg.file as string));
           break;
         }
+
         case "asset": {
           this.assets.set(msg.to as string, msg.source as Uint8Array);
           break;
@@ -136,7 +139,7 @@ const loader: Loader<PostCSSLoaderOptions> = {
     const saferId = (id: string): string => safeId(id, path.basename(this.id));
     const modulesVarName = saferId("modules");
 
-    const output = options.inject ? [`export var ${cssVarName} = ${JSON.stringify(res.css)};`] : [];
+    const output = [`export var ${cssVarName} = ${JSON.stringify(res.css)};`];
     const dts = [`export var ${cssVarName}: string;`];
 
     if (options.namedExports) {
@@ -202,25 +205,28 @@ const loader: Loader<PostCSSLoaderOptions> = {
           output.push(`var ${modulesVarName} = {${getters}};`);
         }
       }
-      const defaultExport = `export default ${supportModules ? modulesVarName : cssVarName};`;
-      output.push(defaultExport);
+    }
 
-      if (options.dts && (await fs.pathExists(this.id))) {
-        if (supportModules)
-          dts.push(
-            `interface ModulesExports ${JSON.stringify(modulesExports)}`,
+    if (!options.inject) output.push(`var ${modulesVarName} = ${JSON.stringify(modulesExports)};`);
 
-            typeof options.inject === "object" && options.inject.treeshakeable
-              ? `interface ModulesExports {inject:()=>void}`
-              : "",
+    const defaultExport = `export default ${supportModules ? modulesVarName : cssVarName};`;
+    output.push(defaultExport);
 
-            `declare const ${modulesVarName}: ModulesExports;`,
-          );
+    if (options.dts && (await fs.pathExists(this.id))) {
+      if (supportModules)
+        dts.push(
+          `interface ModulesExports ${JSON.stringify(modulesExports)}`,
 
-        dts.push(defaultExport);
-        await fs.writeFile(`${this.id}.d.ts`, dts.filter(Boolean).join("\n"));
-      }
-    } //else output.push(`var ${modulesVarName} = ${JSON.stringify(modulesExports)};`);
+          typeof options.inject === "object" && options.inject.treeshakeable
+            ? `interface ModulesExports {inject:()=>void}`
+            : "",
+
+          `declare const ${modulesVarName}: ModulesExports;`,
+        );
+
+      dts.push(defaultExport);
+      await fs.writeFile(`${this.id}.d.ts`, dts.filter(Boolean).join("\n"));
+    }
 
     return { code: output.filter(Boolean).join("\n"), map, extracted };
   },
